18dfaa6061962a2e7fe5349c26c0375fe75bc4d0
Ruubennn20 <rubencouto2002@gmail.com>
2024-09-16 10:36:51 +0100

    ficha_02 ex1

---
+++ Fichas_Java/src/FichaPratica02/EX_02.java
@@ -0,0 +1,10 @@
+package FichaPratica02;
+
+import java.util.Scanner;
+
+public class EX_02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
